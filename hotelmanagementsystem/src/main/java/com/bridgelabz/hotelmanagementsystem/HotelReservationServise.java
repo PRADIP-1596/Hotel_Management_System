e97870d0411836d1f98594c6812fedceca3a9380
PRADIP-1596 <9187pradipyadav@gmail.com>
2021-07-27 01:19:40 +0530

    [Pradip]ADDED:Hotel Management System

---
+++ hotelmanagementsystem/src/main/java/com/bridgelabz/hotelmanagementsystem/HotelReservationServise.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.hotelmanagementsystem;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HotelReservationServise {
+	
+	
+	private List<Hotel> hotelList = new ArrayList<>();
+
+    public void addHotel(Hotel hotel) {
+        hotelList.add(hotel);
+    }
+
+    public List<Hotel> getHotels() {
+        return hotelList;
+    }
+}
---
+++ hotelmanagementsystem/src/test/java/com/bridgelabz/hotelmanagementsystem/HotelReservationTest.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.hotelmanagementsystem;
+
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HotelReservationTest {
+	
+	@Test
+    public void givenHotel_whenInvokeAddHotel_shoulBeAbleToAdd() {
+		
+        HotelReservationServise hotelReservationService = new HotelReservationServise();
+        Hotel hotel = new Hotel("Lakewood",110);
+        hotelReservationService.addHotel(hotel);
+        List<Hotel> hotellist = hotelReservationService.getHotels();
+        Assert.assertTrue(hotellist.contains(hotel));
+    }
+}
